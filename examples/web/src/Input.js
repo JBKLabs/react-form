19abdc449bc45e5f4bd13f84fe3e71c692512690
Josh Hooton <hooton.josh@gmail.com>
2019-09-24 14:41:56 -0500

    Fix Prettier tab spacing.

--- examples/web/src/Input.js
+++ examples/web/src/Input.js
@@ -1,12 +1,7 @@
 import React, { useState } from 'react';
 import { withFormHandling, ValidationError } from '@jbk/react-form';
 
-const Input = ({
-  value,
-  setValue,
-  error,
-  ...remainingProps
-}) => {
+const Input = ({ value, setValue, error, ...remainingProps }) => {
   const [blurred, setBlurred] = useState(false);
 
   return (
@@ -14,25 +9,23 @@ const Input = ({
       <input
         type="text"
         value={value}
-        onChange={e => setValue(e.target.value)}
+        onChange={(e) => setValue(e.target.value)}
         onBlur={() => setBlurred(true)}
         {...remainingProps}
       />
-      {error && blurred && (
-        <div style={{ color: 'red' }}>{error}</div>
-      )}
+      {error && blurred && <div style={{ color: 'red' }}>{error}</div>}
     </>
   );
-}
+};
 
 const onFormValueChange = (value, { name, regex }) => {
   if (regex && !value.match(regex)) {
-    throw new ValidationError('Invalid pattern.')
+    throw new ValidationError('Invalid pattern.');
   } else if (!regex && value === 'hell') {
     throw new ValidationError('HEY!');
   } else if (!regex && value !== 'hello') {
     throw new ValidationError('Too Rude.');
   }
-}
+};
 
 export default withFormHandling(Input, onFormValueChange);
