352d15cd2b7235b29862152285804d664f9c9ca6
Josh Hooton <hooton.josh@gmail.com>
2019-09-24 14:36:49 -0500

    Fix eslint errors.

--- src/util/useDebug.js
+++ src/util/useDebug.js
@@ -1,10 +1,10 @@
 import { useEffect } from 'react';
 
 const useDebug = (name, prop) => {
-	useEffect(() => {
-		// eslint-disable-next-line no-console
-		console.log({ [name]: prop });
-	}, [name, prop]);
+  useEffect(() => {
+    // eslint-disable-next-line no-console
+    console.log({ [name]: prop });
+  }, [name, prop]);
 };
 
 export default useDebug;
