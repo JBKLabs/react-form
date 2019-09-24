4fc153adcbb23313d2c20e0d668b2278cb381a93
Brian Maule <brian.maule@jbknowledge.com>
2019-09-24 14:23:39 -0500

    enable prettier, clean repo

--- src/Form.native.js
+++ src/Form.native.js
@@ -3,11 +3,7 @@ import PropTypes from 'prop-types';
 
 import formFactory from './formFactory';
 
-const FormWrapper = ({ children }) => (
-  <>
-    {children}
-  </>
-);
+const FormWrapper = ({ children }) => <>{children}</>;
 
 FormWrapper.propTypes = { children: PropTypes.any };
 
--- src/withFormHandling.js
+++ src/withFormHandling.js
@@ -2,60 +2,62 @@ import React, { useEffect, useContext, useCallback } from 'react';
 
 import FormContext from './FormContext';
 
-const withFormHandling = (
-	FormInput,
-	onFormChange = () => { },
-) => ({
-	name,
-	defaultValue = '',
-	...remainingProps
+const withFormHandling = (FormInput, onFormChange = () => {}) => ({
+  name,
+  defaultValue = '',
+  ...remainingProps
 }) => {
-	const {
-		values, setValue,
-		errors, setError,
-		keys,
-		setDefault,
-		removeKey
-	} = useContext(FormContext);
+  const {
+    values,
+    setValue,
+    errors,
+    setError,
+    keys,
+    setDefault,
+    removeKey
+  } = useContext(FormContext);
 
-	const value = values[name] || '';
-	const error = errors[name] || null;
-	const key = keys[name] || name;
+  const value = values[name] || '';
+  const error = errors[name] || null;
+  const key = keys[name] || name;
 
-	const setNamedValue = useCallback((nextValue) => {
-		setValue(name, nextValue);
-	}, [name, setValue]);
+  const setNamedValue = useCallback(
+    (nextValue) => {
+      setValue(name, nextValue);
+    },
+    [name, setValue]
+  );
 
-	useEffect(() => {
-		setDefault(name, defaultValue);
-		return () => removeKey(name);
-	}, [name, defaultValue, setDefault, removeKey]);
+  useEffect(() => {
+    setDefault(name, defaultValue);
+    return () => removeKey(name);
+  }, [name, defaultValue, setDefault, removeKey]);
 
-	useEffect(() => {
-		try {
-			onFormChange(value, remainingProps);
-			if (error !== null) {
-				setError(name, null);
-			}
-		} catch (e) {
-			const message = e.displayText || e;
-			if (error !== message) {
-				setError(name, message);
-			}
-		}
-		// eslint-disable-next-line react-hooks/exhaustive-deps
-	}, [value])
+  useEffect(() => {
+    try {
+      onFormChange(value, remainingProps);
+      if (error !== null) {
+        setError(name, null);
+      }
+    } catch (e) {
+      const message = e.displayText || e;
+      if (error !== message) {
+        setError(name, message);
+      }
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [value]);
 
-	return (
-		<FormInput
-			value={value}
-			error={error}
-			setValue={setNamedValue}
-			name={name}
-			{...remainingProps}
-			key={key}
-		/>
-	);
+  return (
+    <FormInput
+      value={value}
+      error={error}
+      setValue={setNamedValue}
+      name={name}
+      {...remainingProps}
+      key={key}
+    />
+  );
 };
 
 export default withFormHandling;
